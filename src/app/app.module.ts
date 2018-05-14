a7c0418621a9fe7914aa1834f39abcc7c01de3fe
Matt Milligan <mattmilligan@Matts-MacBook-Pro.local>
2018-05-14 13:23:17 -0500

    ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
@@ -31,6 +32,7 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    NgbModule.forRoot(),
 
     HttpClientModule,
 
